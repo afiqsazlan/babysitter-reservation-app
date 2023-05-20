48e050d89675d24b767bfa7182742dce70cf26af
Afiq Sazlan <afiqsazlan@gmail.com>
2023-05-20 10:44:11 +0800

    Installed TailwindCSS

--- src/components/reservations/CreateReservation.vue
+++ src/components/reservations/CreateReservation.vue
@@ -4,9 +4,9 @@
 
 <template>
 <form>
-  <div>
-    <label>Name</label>
-    <input>
+  <div class="flex flex-col space-y-2">
+    <label class="text-stale-400 px-1">Name</label>
+    <input class="border border-stale-400 rounded-lg py-1 px-3 ">
   </div>
 </form>
 </template>
