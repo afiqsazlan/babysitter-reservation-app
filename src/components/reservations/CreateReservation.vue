c3a7e38edcc65d420db33fcad2ae27d6554a8189
Afiq Sazlan <afiqsazlan@gmail.com>
2023-05-20 15:21:16 +0800

    Implemented form input bindings

--- src/components/reservations/CreateReservation.vue
+++ src/components/reservations/CreateReservation.vue
@@ -1,5 +1,49 @@
 <script setup lang="ts">
 
+import dayjs from 'dayjs';
+import {reactive} from "vue";
+
+interface Form {
+  name: string;
+  phone: string;
+  address: string;
+  start_at: Date | null;
+  end_at: Date | null;
+  children: Child[];
+}
+
+interface Child {
+  name: string;
+  date_of_birth: Date | null;
+}
+
+const now = dayjs();
+
+const form: Form = reactive({
+  name: "",
+  phone: "",
+  address: "",
+  start_at: null,
+  end_at: null,
+  children: [
+    {
+      name: '',
+      date_of_birth: null,
+    }
+  ]
+});
+
+function handleSubmit() {
+  console.log(form);
+}
+
+function addChild() {
+  form.children.push({
+    name: '',
+    date_of_birth: null
+  })
+}
+
 </script>
 
 <template>
@@ -9,7 +53,7 @@
       you as soon as possible. </p>
 
   </div>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <div class="divide-y">
       <div class="py-8">
         <h2 class="font-medium mb-4">
@@ -18,12 +62,15 @@
         <div class="space-y-2">
           <div class="flex flex-col space-y-2">
             <label class="text-stale-400 px-1">Name</label>
-            <input class="border border-stale-400 rounded-lg py-1 px-3 ">
+            <input v-model="form.name"
+                   class="border border-stale-400 rounded-lg py-1 px-3 ">
           </div>
 
           <div class="flex flex-col space-y-2">
             <label class="text-stale-400 px-1">Phone</label>
-            <input class="border border-stale-400 rounded-lg py-1 px-3 ">
+            <input v-model="form.phone"
+                   class="border border-stale-400 rounded-lg py-1 px-3 "
+            >
           </div>
         </div>
       </div>
@@ -36,38 +83,59 @@
         <div class="space-y-4">
           <div class="flex flex-col col-span-2 space-y-2">
             <label class="text-stale-400 px-1">Address</label>
-            <input class="border border-stale-400 rounded-lg py-1 px-3 ">
+            <input v-model="form.address"
+                   class="border border-stale-400 rounded-lg py-1 px-3 "
+            >
           </div>
           <div class="space-y-2">
             <label>Time</label>
             <div class="grid grid-cols-2 gap-4">
               <div class="flex flex-col space-y-2">
                 <label class="text-stale-400 px-1">Start</label>
-                <input class="border border-stale-400 rounded-lg py-1 px-3 ">
+                <input v-model="form.start_at"
+                       class="border border-stale-400 rounded-lg py-1 px-3 "
+                >
               </div>
               <div class="flex flex-col space-y-2">
                 <label class="text-stale-400 px-1">End</label>
-                <input class="border border-stale-400 rounded-lg py-1 px-3 ">
+                <input v-model="form.end_at"
+                       class="border border-stale-400 rounded-lg py-1 px-3 "
+                >
               </div>
             </div>
           </div>
 
           <div class="space-y-2">
             <label>Children</label>
-            <div class="grid grid-cols-2 gap-4">
+            <div class="flex flex-col ">
+              <template v-for="(child, index) in form.children"
+                        :key="`children-${index}`"
+              >
+                <div class="grid grid-cols-7 items-end space-x-4">
+                  <div class="flex flex-col space-y-2 col-span-4">
+                    <label class="text-stale-400 px-1">
+                      Name
+                    </label>
+                    <input v-model="child.name"
+                           class="border border-stale-400 rounded-lg py-1 px-3 "
+                    >
+                  </div>
+                  <div class="flex flex-col space-y-2 col-span-2  w-full">
+                    <label class="text-stale-400 px-1">Date of Birth</label>
+                    <input type="number"
+                           v-model="child.date_of_birth"
+                           class="border border-stale-400 rounded-lg py-1 px-3 "
+                    >
+                  </div>
+                  <div class="col-span-1 flex justify-end">
+                    <button v-if="index === form.children.length - 1"
+                            @click.prevent="addChild"
+                            class="p-4 bg-green-500 rounded-lg text-2xl text-white"> +
+                    </button>
+                  </div>
+                </div>
+              </template>
 
-              <div class="flex flex-col space-y-2">
-                <label class="text-stale-400 px-1">Name</label>
-                <input type="text"
-                       class="border border-stale-400 rounded-lg py-1 px-3 "
-                >
-              </div>
-              <div class="flex flex-col space-y-2">
-                <label class="text-stale-400 px-1">Age</label>
-                <input type="number"
-                       class="border border-stale-400 rounded-lg py-1 px-3 "
-                >
-              </div>
             </div>
           </div>
         </div>
@@ -75,8 +143,9 @@
     </div>
 
 
-    <div>
-      <button class="bg-blue-500  text-white px-4 py-3 rounded-lg w-full hover:bg-opacity-90 text-lg font-medium tracking-wider">
+    <div class="mt-16 flex flex-row justify-end">
+      <button type="submit"
+              class="bg-blue-500  text-white px-6 py-2 rounded-lg hover:bg-opacity-90 text-lg font-medium tracking-wider">
         SUBMIT
       </button>
     </div>
