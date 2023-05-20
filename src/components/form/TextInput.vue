23744f0a4857439cc1d19d07efd3d86ecad0acfb
Afiq Sazlan <afiqsazlan@gmail.com>
2023-05-20 19:36:01 +0800

    Created reusable TextInput component

---
+++ src/components/form/TextInput.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+interface Props {
+  modelValue: string
+  label: string
+  name: string
+  error?: string
+}
+
+defineProps<Props>()
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="flex flex-col space-y-2">
+    <label :for="name"
+           class="text-stale-400 px-1"
+    >
+      {{ label }}
+    </label>
+    <input :id="name"
+           :value="modelValue"
+           @input="$emit('update:modelValue', $event.target.value)"
+           class="border border-stale-400 rounded-lg py-3 px-3 "
+    />
+
+    <!--  Error message -->
+    <p v-if="error" class="text-red-500 text-sm">{{ error }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/reservations/CreateReservation.vue
+++ src/components/reservations/CreateReservation.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 
 import dayjs from 'dayjs';
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
 import axios from "axios";
+import TextInput from "@/components/form/TextInput.vue";
 
 interface Form {
-  name: string;
-  phone: string;
+  customer_name: string;
+  customer_phone: string;
   address: string;
   start_at: Date | null;
   end_at: Date | null;
@@ -18,11 +19,25 @@ interface Child {
   date_of_birth: Date | null;
 }
 
+interface Error {
+  customer_name: string,
+  customer_phone: string
+  address: string
+  start_at: string
+  end_at: string,
+  children: ChildError[]
+}
+
+interface ChildError {
+  name: string,
+  date_of_birth: string
+}
+
 const now = dayjs();
 
 const form: Form = reactive({
-  name: "",
-  phone: "",
+  customer_name: "",
+  customer_phone: "",
   address: "",
   start_at: null,
   end_at: null,
@@ -34,24 +49,38 @@ const form: Form = reactive({
   ]
 });
 
+const errors = ref<Error>({
+  customer_name: '',
+  customer_phone: '',
+  address: '',
+  start_at: '',
+  end_at: '',
+  children: []
+});
+
 async function handleSubmit() {
   console.log(form);
 
+  resetErrors();
 
   axios.defaults.withCredentials = true;
 
   const apiBaseUrl = import.meta.env.VITE_API_BASE;
   const apiUrl = `${apiBaseUrl}/api`
 
-   await axios.get(`${apiBaseUrl}/sanctum/csrf-cookie`);
+  await axios.get(`${apiBaseUrl}/sanctum/csrf-cookie`);
 
   try {
-    const response = await axios.post(`${apiUrl}/reservations`);
+    const response = await axios.post(`${apiUrl}/reservations`, form);
     console.log({response})
     return response.data;
   } catch (error) {
-    console.error('Error', error.response.data);
-    throw error;
+    if (error.response.status === 422) {
+      console.log({
+        messages: error.response.data.errors
+      })
+      handleValidationErrorMessages(error.response.data.errors);
+    }
   }
 }
 
@@ -60,6 +89,38 @@ function addChild() {
     name: '',
     date_of_birth: null
   })
+}
+
+function handleValidationErrorMessages(errorMessages) {
+  const fieldMappings = {
+    customer_name: 'customer_name',
+    customer_phone: 'customer_phone',
+    address: 'address',
+    start_at: 'start_at',
+    children: 'children'
+  };
+
+  Object.entries(errorMessages).forEach(([key, value]) => {
+    const field = fieldMappings[key];
+    if (field) {
+      errors.value[field] = value[0];
+    }
+  });
+
+  console.log({
+    errors: errors.value
+  })
+}
+
+function resetErrors() {
+  errors.value = {
+    customer_name: '',
+    customer_phone: '',
+    address: '',
+    start_at: '',
+    end_at: '',
+    children: []
+  }
 }
 
 </script>
@@ -78,18 +139,16 @@ function addChild() {
           Your Contact Details
         </h2>
         <div class="space-y-2">
-          <div class="flex flex-col space-y-2">
-            <label class="text-stale-400 px-1">Name</label>
-            <input v-model="form.name"
-                   class="border border-stale-400 rounded-lg py-1 px-3 ">
-          </div>
-
-          <div class="flex flex-col space-y-2">
-            <label class="text-stale-400 px-1">Phone</label>
-            <input v-model="form.phone"
-                   class="border border-stale-400 rounded-lg py-1 px-3 "
-            >
-          </div>
+          <text-input v-model="form.customer_name"
+                      label="Name"
+                      name="name"
+                      :error="errors.customer_name"
+          ></text-input>
+          <text-input v-model="form.customer_phone"
+                      label="Phone"
+                      name="phone"
+                      :error="errors.customer_phone"
+          ></text-input>
         </div>
       </div>
 
@@ -99,12 +158,11 @@ function addChild() {
         </h2>
 
         <div class="space-y-4">
-          <div class="flex flex-col col-span-2 space-y-2">
-            <label class="text-stale-400 px-1">Address</label>
-            <input v-model="form.address"
-                   class="border border-stale-400 rounded-lg py-1 px-3 "
-            >
-          </div>
+          <text-input v-model="form.address"
+                      label="Address"
+                      name="address"
+                      :error="errors.address"
+          ></text-input>
           <div class="space-y-2">
             <label>Time</label>
             <div class="grid grid-cols-2 gap-4">
@@ -130,14 +188,13 @@ function addChild() {
                         :key="`children-${index}`"
               >
                 <div class="grid grid-cols-7 items-end space-x-4">
-                  <div class="flex flex-col space-y-2 col-span-4">
-                    <label class="text-stale-400 px-1">
-                      Name
-                    </label>
-                    <input v-model="child.name"
-                           class="border border-stale-400 rounded-lg py-1 px-3 "
-                    >
-                  </div>
+                  <text-input v-model="form.address"
+                              label="Name"
+                              :name="`child_${index}_name`"
+                              :error="errors.children[index]?.name"
+                              class="col-span-4"
+                  ></text-input>
+
                   <div class="flex flex-col space-y-2 col-span-2  w-full">
                     <label class="text-stale-400 px-1">Date of Birth</label>
                     <input type="number"
