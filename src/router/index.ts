4970568dc4ecae07948f13107222e8f69730b2c8
Afiq Sazlan <afiqsazlan@gmail.com>
2023-05-22 05:18:29 +0800

    Extracted Create Reservation form into new route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ReservationShow from "@/views/Reservation/ReservationShow.vue";
+import ReservationCreate from "@/views/Reservation/ReservationCreate.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: HomeView
+        },
+        {
+            path: '/reservations/create',
+            name: 'reservations.create',
+            component: ReservationCreate
         },
         {
             path: '/reservations/:reservationRefNo',
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import CreateReservation from "@/components/reservations/CreateReservation.vue";
 </script>
 
 <template>
-  <main>
-    <create-reservation></create-reservation>
-  </main>
+  <div class="w-screen  h-screen flex flex-col py-16 items-center justify-center">
+
+    <h1 class="font-bold text-lg md:text-2xl"> Are you looking for a babysitter? </h1>
+    <router-link :to="{ name: 'reservations.create' }"
+                 class="bg-blue-500 mt-6 py-4 px-6 rounded-lg md:text-lg tracking-wider text-white hover:bg-opacity-90"
+    >
+      Make a reservation!
+    </router-link>
+  </div>
 </template>
